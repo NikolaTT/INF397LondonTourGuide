a1c0842a29d5bbb1d7df13db7ebcf276d68aaf06
Nikola Toshev <nekavsitam@gmail.com>
2017-03-21 01:19:25 +0200

    I broke git extremely bad. After many manual changes, the app is working again .

--- app/src/main/java/aubg/edu/londontourguide/InformationFragment.java
+++ app/src/main/java/aubg/edu/londontourguide/InformationFragment.java
@@ -1,21 +1,12 @@
 package aubg.edu.londontourguide;
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import android.database.Cursor;
-=======
 import android.content.Intent;
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
-=======
-import android.content.Intent;
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-<<<<<<< HEAD
-<<<<<<< HEAD
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -30,30 +21,27 @@ import java.util.ArrayList;
 
 import aubg.edu.londontourguide.adapters.NewsAdapter;
 import aubg.edu.londontourguide.data.NewsContract;
-=======
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
-=======
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class InformationFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
+public class InformationFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final int INFORMATION_LOADER = 0;
 
-    private static final String[] NEWS_COLUMNS = new String[] {
+    private static final String[] NEWS_COLUMNS = new String[]{
             NewsContract.NewsEntry._ID,
             NewsContract.NewsEntry.COLUMN_TITLE,
             NewsContract.NewsEntry.COLUMN_URL
@@ -63,20 +51,14 @@ public class InformationFragment extends Fragment implements LoaderManager.Loade
     private static final int INDEX_NEWS_TITLE = 1;
     private static final int INDEX_NEWS_URL = 2;
 
-<<<<<<< HEAD
     private Uri mUri = NewsContract.NewsEntry.CONTENT_URI;
 
-<<<<<<< HEAD
     private RecyclerView mRecyclerView;
     private NewsAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
-=======
-Button btnVideo;
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
-=======
-Button btnVideo;
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
+    Button btnVideo;
+
     public InformationFragment() {
         // Required empty public constructor
     }
@@ -86,7 +68,7 @@ Button btnVideo;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_information, container, false);
+        return inflater.inflate(R.layout.fragment_information, container, false);
     }
 
     @Override
@@ -103,10 +85,21 @@ Button btnVideo;
 
         mAdapter = new NewsAdapter(getActivity());
         mRecyclerView.setAdapter(mAdapter);
+
+        btnVideo = (Button) view.findViewById(R.id.btnVideo);
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=jiz0uJaFFII")));
+                Log.i("Video", "Video Playing....");
+
+            }
+        });
+
     }
 
     @Override
-<<<<<<< HEAD
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         getLoaderManager().initLoader(INFORMATION_LOADER, null, this);
         super.onActivityCreated(savedInstanceState);
@@ -114,7 +107,7 @@ Button btnVideo;
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        if ( null != mUri ) {
+        if (null != mUri) {
             // Now create and return a CursorLoader that will take care of
             // creating a Cursor for the data being displayed.
             return new CursorLoader(
@@ -136,28 +129,7 @@ Button btnVideo;
     }
 
     @Override
-<<<<<<< HEAD
     public void onLoaderReset(Loader<Cursor> loader) {
         mAdapter.deleteAllData();
-=======
-=======
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        btnVideo = (Button)view.findViewById(R.id.btnVideo);
-        btnVideo.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=jiz0uJaFFII")));
-                Log.i("Video", "Video Playing....");
-
-            }
-        });
-<<<<<<< HEAD
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
-=======
->>>>>>> 8dc89d73508f7752debf892308e71ddeaf3cf412
     }
 }
